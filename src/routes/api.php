bd856ec50e7e21a8f80c4de1d6a3f3d5c0cf409b
gomidx <lucaspavao2002@gmail.com>
2024-02-18 12:24:56 -0300

    creating routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,19 +1,26 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
-|--------------------------------------------------------------------------
+|--------------------------------------------------------------------------a
 | API Routes
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
 |
 */
 
-Route::middleware("auth:sanctum")->get("/user", function (Request $request) {
-    return $request->user();
+Route::post('/token', [AuthController::class, 'generateToken'])->name('auth.generateToken');
+Route::post('/user', [UserController::class, 'store'])->name('user.store');
+Route::get('/user/{id}', [UserController::class, 'show'])->name('user.show');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('auth.logout');
+
+    Route::apiResource('user', UserController::class)->except(['store', 'show']);
 });
