354c144597b74f9d5742855593d5dc713426e5f1
gomidx <lucaspavao2002@gmail.com>
2024-02-18 12:21:30 -0300

    http helper

---
+++ src/app/Helpers/Http.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Helpers;
+
+use Illuminate\Http\Response;
+
+trait Http
+{
+    private function ok(mixed $data): array
+    {
+        return $this->buildResponse(Response::HTTP_OK, $data);
+    }
+
+    private function created(mixed $data): array
+    {
+        return $this->buildResponse(Response::HTTP_CREATED, $data);
+    }
+
+    private function forbidden(mixed $data): array
+    {
+        return $this->buildResponse(Response::HTTP_FORBIDDEN, $data);
+    }
+
+    private function notFound(mixed $data): array
+    {
+        return $this->buildResponse(Response::HTTP_NOT_FOUND, $data);
+    }
+
+    private function unprocessableEntity(mixed $data): array
+    {
+        return $this->buildResponse(Response::HTTP_UNPROCESSABLE_ENTITY, $data);
+    }
+
+    private function serverError(): array
+    {
+        return $this->buildResponse(Response::HTTP_INTERNAL_SERVER_ERROR, 'An internal error has occurred, please contact an administrator.');
+    }
+
+    private function buildResponse(int $code, mixed $data): array
+    {
+        return [
+            'code' => $code,
+            'response' => [
+                'data' => $data
+            ]
+        ];
+    }
+}
